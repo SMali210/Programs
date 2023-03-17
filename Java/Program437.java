27b87b629010707eab491031203fff1428c0e2c5
SMali210 <121398030+SMali210@users.noreply.github.com>
2023-03-17 12:58:12 +0530

    Add files via upload

---
+++ Java/Program437.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Program437
+{
+      public static void main(String arg[])
+      {
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter The Size Of Array");
+            int Size = sobj.nextInt();
+
+            Sorting obj = new Sorting(Size);
+
+            obj.Accept();
+            obj.Display();
+      }
+}
+interface Marvellous
+{
+      public void BubbleSort();
+      public void SelectionSort();
+      public void InsertionSort();
+}
+
+class Sorting implements Marvellous
+{
+      private int Arr[];
+
+      public Sorting(int Size)
+      {
+            Arr = new int[Size];
+      }
+
+      public void Accept()
+      {
+            Scanner sobj = new Scanner(System.in);
+            System.out.println("Enter The Elements");
+
+            for(int i = 0;i<Arr.length;i++)
+            {
+                  Arr[i] = sobj.nextInt();
+            }
+  
+      }
+      public void Display()
+      {
+            System.out.println("Elements of Array are :");
+
+            for(int i = 0;i<Arr.length;i++)
+            {
+                  System.out.println(Arr[i]);
+            }
+      }
+
+      public void BubbleSort()
+      {
+
+      }
+      public void InsertionSort()
+      {
+
+      }
+      public void SelectionSort()
+      {
+            
+      }
+}
